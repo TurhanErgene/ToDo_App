97c217efef39747f5089c02f0f77e7b83db2b310
Turhan <turhan-ergene@hotmail.com>
2021-12-18 23:56:59 +0100

    Form component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <h1>To Do App With React</h1>
+      <header>
+        <h1>To Do App With React</h1>
+      </header>
+      <Form/>
     </div>
   );
 }
