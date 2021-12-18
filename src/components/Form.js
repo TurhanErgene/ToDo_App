d5e0d7f2e2e9a79da649fb3469bc2c5326b965b1
Turhan <turhan-ergene@hotmail.com>
2021-12-19 00:05:04 +0100

    Todo list created

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Form = () => {
   return (
     <div>
-       <form>
-      <input type="text" class="todo-input" />
-      <button class="todo-button" type="submit">
-        <i class="fas fa-plus-square"></i>
-      </button>
-      <div class="select">
-        <select name="todos" class="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
-    </form>
+      <form>
+        <input type="text" className="todo-input" />
+        <button className="todo-button" type="submit">
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
